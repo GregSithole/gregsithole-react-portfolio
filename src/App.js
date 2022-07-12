414a6d002e20dbda1158b71c26f952a4b9404c83
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-12 20:57:46 +0200

    Added Navigation to App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+// import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
     return (
-        <div>
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+            <div>
 
+            </div>
         </div>
     )
 }
