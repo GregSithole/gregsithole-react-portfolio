455e0e31555ec5f23b38a3c0ddf735ffad746860
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-13 20:23:03 +0200

    Added Home to App Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
