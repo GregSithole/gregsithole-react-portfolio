95f12f960d2cb376d9c2a921c250e19669147042
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-13 20:22:52 +0200

    Added Home.jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HiArrowNarrowRight } from 'react-icons/hi';
+
+const Home = () => {
+    return (
+        <div name='home' className='w-full h-screen bg-[#202020]'>
+
+        </div>
+    )
+}
+
+export default Home
