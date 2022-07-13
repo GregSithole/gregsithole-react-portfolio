224577d3c49b64e0bc4815ed11387c68a4afbb7a
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-13 21:30:51 +0200

    Added correct colour to About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
             <div className='flex flex-col justify-center items-center w-full h-full'>
                 <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
                     <div className='sm:text-right pb-8 pl-4'>
-                        <p className='text-4xl font-bold inline border-b-4 border-pink-600'>About</p>
+                        <p className='text-4xl font-bold inline border-b-4 border-[#ffa500]'>About</p>
                     </div>
                     <div>
 
