fee52f3438f65591e718cf6cbc1b0912678072ab
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-13 20:22:16 +0200

    Added custom colours to Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'social': '#393939'
+      },
+    },
   },
   plugins: [],
 }
