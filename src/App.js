9097ef4b49c51955f63fa7aba99239370dbc79d4
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-17 20:19:42 +0200

    Removed Previous Components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import About from "./components/About";
-import Experience from "./components/Experience";
-import Skills from "./components/Skills";
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Home />
-      <About />
-      <Experience />
-      <Skills />
-    </div>
-  );
+    return (
+        <div>App</div>
+    );
 }
 
 export default App;
