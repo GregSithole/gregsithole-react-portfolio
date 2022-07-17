d22a174c82749a2580564f019ba681aeb247d212
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-17 20:15:21 +0200

    Further Updates to Project

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,9 @@
 
 import logo from '../assets/logo.svg';
+import profile from '../assets/greg.jpeg';
 
 /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
     logo,
+    profile
 };
