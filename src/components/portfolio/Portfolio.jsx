8373b7e0c287983452aeba10818bd0e1893158fa
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-17 20:28:15 +0200

    Added Resume & Portfolio Components

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Portfolio.css';
+
+const Portfolio = () => {
+    return (
+        <div>Portfolio</div>
+    )
+}
+
+export default Portfolio
---
+++ src/components/resume/Resume.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Resume.css';
+
+const Resume = () => {
+    return (
+        <div>Resume</div>
+    )
+}
+
+export default Resume
