392f96b56cb38c425ca06c1e7aca929b2d83d30e
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-17 22:12:18 +0200

    Added Basic Home Page Content

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './Home.css';
+import Me from '../../assets/avatar-1.svg'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className="home container" id='home'>
+            <div className="intro">
+                <img src={Me} alt="" className='home__img' />
+                <h1 className="home__name">Greg Sithole</h1>
+                <span className="home__education">I'm a software Engineer</span>
+            </div>
+        </section>
     )
 }
 
