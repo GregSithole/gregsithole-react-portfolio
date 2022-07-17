2a711bd246dc314f06c374fb8a552b27a91ccc6b
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-17 20:41:57 +0200

    Added all components to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import './App.css';
+import Sidebar from './components/sidebar/Sidebar';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Services from './components/services/Services';
+import Resume from './components/resume/Resume';
+import Portfolio from './components/portfolio/Portfolio';
+import Pricing from './components/pricing/Pricing';
+import Testimonials from './components/testimonials/Testimonials';
+import Blog from './components/blog/Blog';
+import Contact from './components/contact/Contact';
 
 function App() {
     return (
-        <div>App</div>
+        <>
+            <Sidebar />
+            <main className='main'>
+                <Home />
+                <About />
+                <Services />
+                <Resume />
+                <Portfolio />
+                <Pricing />
+                <Testimonials />
+                <Blog />
+                <Contact />
+            </main>
+        </>
     );
 }
 
