985c7b53a0bea40f9236a164dbb092534abb1529
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-18 22:09:50 +0200

    Updated Services Component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 import './Services.css';
+import Image1 from '../../assets/service-1.svg'
+import Image2 from '../../assets/service-2.svg'
+import Image3 from '../../assets/service-3.svg'
+
+const data = [
+    {
+        id: 1,
+        image: Image1,
+        title: "UI/UX design",
+        description:
+            "Lorem ipsum dolor sit amet consectetuer adipiscing elit         aenean commodo ligula eget.",
+    },
+    {
+        id: 2,
+        image: Image2,
+        title: "Web Development",
+        description:
+            "Lorem ipsum dolor sit amet consectetuer adipiscing elit         aenean commodo ligula eget.",
+    },
+    {
+        id: 3,
+        image: Image3,
+        title: "Photography",
+        description:
+            "Lorem ipsum dolor sit amet consectetuer adipiscing elit         aenean commodo ligula eget.",
+    },
+];
 
 const Services = () => {
     return (
-        <div>Services</div>
+        <section className="services container section">
+            <h2 className="section__title">Services</h2>
+
+            <div className="services__container grid">
+                {data.map(({ id, image, title, description }) => {
+                    return (
+                        <div className="services__card" key={id}>
+                            <img src={image} alt='' className='services__img' />
+
+                            <h3 className="services__title">{title}</h3>
+                            <p className="services__description">{description}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        </section>
     )
 }
 
