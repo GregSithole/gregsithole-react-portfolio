5bc6b3757a1da80cd8dbbec05c329c9b39cea367
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-18 05:38:43 +0200

    Updated About Component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,69 @@
 import React from 'react';
 import './About.css';
+import Image from '../../assets/avatar-2.svg';
+import AboutBox from './AboutBox';
 
 const About = () => {
     return (
-        <div>About</div>
+        <section className="about container section" id="about">
+            <h2 className="section__title">About Me </h2>
+
+            <div className="about__container grid">
+                <img src={Image} alt="" className='about__img' />
+
+                <div className="about__data grid">
+                    <div className="about__info">
+                        <p className="about__description">
+                            I am Greg Sithole, Software Engineer from Johannesburg, South Africa.
+                        </p>
+                        <a href="" className="btn">Donwload CV</a>
+                    </div>
+
+                    <div className="about__skills grid">
+                        <div className="skills__data">
+                            <div className="skills__titles">
+                                <h3 className="skills__name">Development</h3>
+                                <span className="skills__number">90%</span>
+                            </div>
+
+                            <div className="skills__bar">
+                                <span className="skills__percentage development">
+
+                                </span>
+                            </div>
+                        </div>
+
+                        <div className="skills__data">
+                            <div className="skills__titles">
+                                <h3 className="skills__name">UI/UX Design</h3>
+                                <span className="skills__number">80%</span>
+                            </div>
+
+                            <div className="skills__bar">
+                                <span className="skills__percentage ui__design">
+
+                                </span>
+                            </div>
+                        </div>
+
+                        <div className="skills__data">
+                            <div className="skills__titles">
+                                <h3 className="skills__name">Photography</h3>
+                                <span className="skills__number">60%</span>
+                            </div>
+
+                            <div className="skills__bar">
+                                <span className="skills__percentage photography">
+
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <AboutBox />
+        </section>
     )
 }
 
---
+++ src/components/About/AboutBox.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { RiFireLine, RiCupLine, RiGroupLine, RiTrophyLine } from 'react-icons/ri';
+
+const AboutBox = () => {
+    return (
+        <div className="about__boxes grid">
+            <div className="about__box">
+                <RiFireLine className='about__icon' />
+
+                <div>
+                    <h3 className="about__title">198</h3>
+                    <span className="about__subtitle">Projects Completed</span>
+                </div>
+            </div>
+
+            <div className="about__box">
+                <RiCupLine className='about__icon' />
+
+                <div>
+                    <h3 className="about__title">5660</h3>
+                    <span className="about__subtitle">Cups of Coffee</span>
+                </div>
+            </div>
+
+            <div className="about__box">
+                <RiGroupLine className='about__icon' />
+
+                <div>
+                    <h3 className="about__title">427</h3>
+                    <span className="about__subtitle">Satisifed Clients</span>
+                </div>
+            </div>
+
+            <div className="about__box">
+                <RiTrophyLine className='about__icon' />
+
+                <div>
+                    <h3 className="about__title">35</h3>
+                    <span className="about__subtitle">Nominees Winner</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutBox
