d316b14289e23d317b9200d9b87911375d33f2ae
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-18 23:39:28 +0200

    Added Resume Information

---
+++ src/components/resume/Data.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+
+const Data = [
+    {
+        id: 1,
+        category: "education",
+        icon: "graduation",
+        year: "2013 - 2016",
+        title: "Varsity College",
+        subtitle: "IIE Diploma in Information Technology in Software Development",
+        desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto",
+    },
+    {
+        id: 2,
+        category: "education",
+        icon: "graduation",
+        year: "2008 - 2012",
+        title: "Summit College",
+        subtitle: "National Senior Certificate",
+        desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto",
+    },
+    {
+        id: 3,
+        category: "experience",
+        icon: "briefcase",
+        year: "2022 - present",
+        title: "Kurtosys",
+        subtitle: "Software Engineer",
+        desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto",
+    },
+    {
+        id: 4,
+        category: "experience",
+        icon: "briefcase",
+        year: "2017 - 2021",
+        title: "Entrostat",
+        subtitle: "Junior Software Developer",
+        desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
+    },
+    {
+        id: 5,
+        category: "experience",
+        icon: "briefcase",
+        year: "2016 - 2017",
+        title: "Empire State",
+        subtitle: "Junior Software Developer",
+        desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
+    },
+    {
+        id: 6,
+        category: "experience",
+        icon: "briefcase",
+        year: "2015 - 2016",
+        title: "The Digital Academy",
+        subtitle: "Software Development Intern",
+        desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
+    },
+    {
+        id: 7,
+        category: "experience",
+        icon: "briefcase",
+        year: "2015",
+        title: "Varisty College",
+        subtitle: "IT Support Assistant",
+        desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
+    },
+    {
+        id: 8,
+        category: "experience",
+        icon: "briefcase",
+        year: "2014 - 2015",
+        title: "Varisty College",
+        subtitle: "Student Ambassador",
+        desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci     ad nec admodum perfecto.",
+    },
+
+];
+
+export default Data
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import './Resume.css';
+import Data from './Data';
+import Card from './Card';
 
 const Resume = () => {
     return (
-        <div>Resume</div>
+        <section className="resume container section" id="resume">
+            <h2 className="section__title">Experience</h2>
+
+            <div className="resume__container grid">
+                <div className="timeline grid">
+                    {Data.map((val, id) => {
+                        if (val.category === 'education') {
+                            return (
+                                <Card key={id} icon={val.icon} title={val.title} year={val.year} desc={val.desc} category={val.category} />
+                            );
+                        }
+                    })}
+                </div>
+
+                <div className="timeline grid">
+                    {Data.map((val, id) => {
+                        if (val.category === 'experience') {
+                            return (
+                                <Card key={id} icon={val.icon} title={val.title} year={val.year} desc={val.desc} category={val.category} />
+                            )
+                        }
+                    })}
+                </div>
+            </div>
+        </section>
     )
 }
 
