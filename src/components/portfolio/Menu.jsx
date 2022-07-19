0a177df3d68765c313e7618b39211a59e7138233
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-19 22:15:50 +0200

    Updated Portfolio

---
+++ src/components/portfolio/Menu.jsx
@@ -0,0 +1,47 @@
+import Work1 from "../../assets/work-1.svg";
+import Work2 from "../../assets/work-2.svg";
+import Work3 from "../../assets/work-3.svg";
+import Work4 from "../../assets/work-4.svg";
+import Work5 from "../../assets/work-5.svg";
+import Work6 from "../../assets/work-6.svg";
+
+const Menu = [
+    {
+        id: 1,
+        image: Work1,
+        title: "Project Management Illustration",
+        category: "Design"
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: "Guest App Walkthrough Screens",
+        category: "Art"
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: "Delivery App Wireframe",
+        category: "Branding"
+    },
+    {
+        id: 4,
+        image: Work4,
+        title: "Onboarding Motivation",
+        category: "Design"
+    },
+    {
+        id: 5,
+        image: Work5,
+        title: "iMac Mockup Design",
+        category: "Creative"
+    },
+    {
+        id: 6,
+        image: Work6,
+        title: "Game Store App Concept",
+        category: "Art"
+    },
+];
+
+export default Menu;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Portfolio.css';
+import Menu from './Menu';
+import { RiLink } from 'react-icons/ri';
 
 const Portfolio = () => {
+
+    const [items, setItems] = useState(Menu);
+    const filterItems = (categoryItem) => {
+        const updatedItems = Menu.filter((curElem) => {
+            return curElem.category === categoryItem;
+        });
+
+        setItems(updatedItems);
+    }
+
     return (
-        <div>Portfolio</div>
+        <section className="work container section" id="work">
+            <h2 className="section__title">
+                Recent Projects
+            </h2>
+
+            <div className="work__filters">
+                <span className="work__item" onClick={() => setItems(Menu)}>Everything</span>
+                <span className="work__item" onClick={() => filterItems('Creative')}>Creative</span>
+                <span className="work__item" onClick={() => filterItems('Art')}>Art</span>
+                <span className="work__item" onClick={() => filterItems('Design')}>Design</span>
+                <span className="work__item" onClick={() => filterItems('Branding')}>Branding</span>
+            </div>
+
+            <div className="work__container grid">
+                {items.map((elem) => {
+                    const { id, image, title, category } = elem;
+
+                    return (
+                        <div className="work__card" key={id}>
+                            <div className="work__thumbnail">
+                                <img src={image} alt="" className="work__img" />
+                                <div className="work__mask"></div>
+                            </div>
+
+                            <span className="work__category">{category}</span>
+                            <h3 className="work__title">{title}</h3>
+                            <a href="#" className="work__button">
+                                <RiLink className='work__button-icon' />
+                            </a>
+                        </div>
+                    )
+                })}
+            </div>
+        </section>
     )
 }
 
