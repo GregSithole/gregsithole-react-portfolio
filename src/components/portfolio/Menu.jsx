e14150c130dff7d027e3dff8a8c18b385932d858
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-25 23:35:34 +0200

    Updated Projects to Match my projects

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -1,47 +1,32 @@
 import Work1 from "../../assets/work-1.svg";
 import Work2 from "../../assets/work-2.svg";
 import Work3 from "../../assets/work-3.svg";
-import Work4 from "../../assets/work-4.svg";
-import Work5 from "../../assets/work-5.svg";
-import Work6 from "../../assets/work-6.svg";
 
 const Menu = [
     {
         id: 1,
         image: Work1,
-        title: "Project Management Illustration",
-        category: "Design"
+        title: "Memories App",
+        category: ["Frontend", "React"],
+        url: "https://memory-app.gregsithole.com",
+        repositoryUrl: "https://github.com/GregSithole/memories-project"
     },
     {
         id: 2,
         image: Work2,
-        title: "Guest App Walkthrough Screens",
-        category: "Art"
+        title: "Cryptoverse",
+        category: ["Frontend", "React"],
+        url: "https://cryptoverse.gregsithole.com",
+        repositoryUrl: "https://github.com/GregSithole/crypto-app"
     },
     {
         id: 3,
         image: Work3,
-        title: "Delivery App Wireframe",
-        category: "Branding"
-    },
-    {
-        id: 4,
-        image: Work4,
-        title: "Onboarding Motivation",
-        category: "Design"
-    },
-    {
-        id: 5,
-        image: Work5,
-        title: "iMac Mockup Design",
-        category: "Creative"
-    },
-    {
-        id: 6,
-        image: Work6,
-        title: "Game Store App Concept",
-        category: "Art"
-    },
+        title: "Travel Advisor",
+        category: ["Frontend"],
+        url: "https://travel.gregsithole.com",
+        repositoryUrl: "https://github.com/GregSithole/travel-advisor"
+    }
 ];
 
 export default Menu;
