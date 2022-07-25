5cfa080ef8bb6b2d4cb721ec94eb3b757128eb28
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-25 17:30:07 +0200

    Added Responsive Design to Website

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
 
                 <HeaderSocials />
 
-                <a href="#contact" className="btn"> Hire Me</a>
+                <a href="#contact" className="btn"> Contact Me</a>
 
                 <ScrollDown />
             </div>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css";
 import Logo from "../../assets/logo.svg";
 import {
@@ -11,70 +11,79 @@ import {
     RiFileList3Line,
     RiMoonLine,
     RiSunLine,
+    RiMenu2Line,
 } from "react-icons/ri";
 
 
 const Sidebar = (props) => {
+    const [toggle, showMenu] = useState(false);
+
     return (
-        <aside className="aside">
-            <a href="#home" className="nav__logo">
-                <img src={Logo} alt="logo" />
-            </a>
+        <>
+            <aside className={toggle ? 'aside show-menu' : 'aside'}>
+                <a href="#home" className="nav__logo">
+                    <img src={Logo} alt="logo" />
+                </a>
 
-            <nav className="nav">
-                <div className="nav__menu">
-                    <ul className="nav__list">
-                        <li className="nav__item">
-                            <a href="#home" className="nav__link">
-                                <RiHome2Line />
-                            </a>
-                        </li>
+                <nav className="nav">
+                    <div className="nav__menu">
+                        <ul className="nav__list">
+                            <li className="nav__item">
+                                <a href="#home" className="nav__link">
+                                    <RiHome2Line />
+                                </a>
+                            </li>
 
-                        <li className="nav__item">
-                            <a href="#about" className="nav__link">
-                                <RiUser3Line />
-                            </a>
-                        </li>
+                            <li className="nav__item">
+                                <a href="#about" className="nav__link">
+                                    <RiUser3Line />
+                                </a>
+                            </li>
 
-                        <li className="nav__item">
-                            <a href="#services" className="nav__link">
-                                <RiFileList3Line />
-                            </a>
-                        </li>
+                            <li className="nav__item">
+                                <a href="#services" className="nav__link">
+                                    <RiFileList3Line />
+                                </a>
+                            </li>
 
-                        <li className="nav__item">
-                            <a href="#resume" className="nav__link">
-                                <RiBriefcase2Line />
-                            </a>
-                        </li>
+                            <li className="nav__item">
+                                <a href="#resume" className="nav__link">
+                                    <RiBriefcase2Line />
+                                </a>
+                            </li>
 
-                        <li className="nav__item">
-                            <a href="#portfolio" className="nav__link">
-                                <RiStackLine />
-                            </a>
-                        </li>
+                            <li className="nav__item">
+                                <a href="#portfolio" className="nav__link">
+                                    <RiStackLine />
+                                </a>
+                            </li>
 
-                        <li className="nav__item">
-                            <a href="#blog" className="nav__link">
-                                <RiDraftLine />
-                            </a>
-                        </li>
+                            <li className="nav__item">
+                                <a href="#blog" className="nav__link">
+                                    <RiDraftLine />
+                                </a>
+                            </li>
 
-                        <li className="nav__item">
-                            <a href="#contact" className="nav__link">
-                                <RiChat3Line />
-                            </a>
-                        </li>
-                    </ul>
+                            <li className="nav__item">
+                                <a href="#contact" className="nav__link">
+                                    <RiChat3Line />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+                <div className="nav__footer">
+                    <button onClick={() => { props.switchTheme(); showMenu(!toggle) }} className="nav__link footer__button">
+                        {props.theme === 'light' ? <RiMoonLine /> : <RiSunLine />}
+                    </button>
                 </div>
-            </nav>
+            </aside>
 
-            <div className="nav__footer">
-                <button onClick={props.switchTheme} className="nav__link footer__button">
-                    {props.theme === 'light' ? <RiMoonLine /> : <RiSunLine />}
-                </button>
+            <div className={toggle ? 'nav__toggle nav__toggle-open' : 'nav__toggle'} onClick={() => showMenu(!toggle)}>
+                <RiMenu2Line />
             </div>
-        </aside>
+        </>
     );
 };
 
