2b1969027c6b39d218cb7de77db4e2256fb19e1d
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-26 00:00:55 +0200

    Removed Pricing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import About from './components/about/About';
 import Services from './components/services/Services';
 import Resume from './components/resume/Resume';
 import Portfolio from './components/portfolio/Portfolio';
-import Pricing from './components/pricing/Pricing';
 import Testimonials from './components/testimonials/Testimonials';
 import Blog from './components/blog/Blog';
 import Contact from './components/contact/Contact';
