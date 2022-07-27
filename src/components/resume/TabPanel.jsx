3497d8d891068d44c4b6433adbc238c6c195402f
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-27 22:46:30 +0200

    Changed TabPanel to Use Framer Motion

--- src/components/resume/TabPanel.jsx
+++ src/components/resume/TabPanel.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
 const TabPanel = ({ _isActive, children }) => {
     return (
-        <div className={`tab__panel${_isActive ? " is-active" : ""}`}>
+        <motion.div
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            className={`tab__panel${_isActive ? " is-active" : ""}`}>
             {children}
-        </div>
-    )
-}
+        </motion.div>
+    );
+};
 
-export default TabPanel
+export default TabPanel;
