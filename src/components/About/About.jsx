f18746bda3e6db8d338035ed0673e04b7579d063
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-30 23:24:13 +0200

    Changed about to only include a sing section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -20,7 +20,9 @@ const About = () => {
                 <div className="about__data grid">
                     <div className="about__info">
                         <p className="about__description">
-                            Hello! I'm Greg<br /><br />
+                            Hello! I'm Greg, and I'm based in Johannesburg, South Africa. I studied at Varisty College Sandton, where I completed my diploma in Software Development.<br /><br />
+                            Fastforward to today, and I can honestly say it's been a beautiful journery and I hope to grow even further as Software Engineer. My current focus these days
+                            is on expanidng my portfolio by building more projects that I can add on here, as well as being a great team player at my current role.<br /><br />
                             Here are a few technologies I’ve been working with recently:
                         </p>
                         <ul className="about__list">
@@ -34,7 +36,7 @@ const About = () => {
                         <button className="btn" onClick={downloadResume}>Donwload CV</button>
                     </div>
 
-                    <div className="about__skills grid">
+                    {/* <div className="about__skills grid">
                         <div className="skills__data">
                             <div className="skills__titles">
                                 <h3 className="skills__name">Development</h3>
@@ -73,7 +75,7 @@ const About = () => {
                                 </span>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
 
