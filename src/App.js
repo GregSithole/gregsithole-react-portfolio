978ee69c584e88f01b39cf14b4fa4361231a61dd
Greg Sithole <gregsithole@hotmail.co.za>
2022-08-01 22:45:42 +0200

    Changed foldername

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 import Sidebar from './components/sidebar/Sidebar';
 import Home from './components/home/Home';
-import About from './components/about/About';
+import About from './components/about1/About';
 import Services from './components/services/Services';
 import Resume from './components/resume/Resume';
 import Portfolio from './components/portfolio/Portfolio';
