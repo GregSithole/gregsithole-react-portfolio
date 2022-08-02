3355d5ecdf1042f084313f7b1e3cc1131e44bc89
Greg Sithole <gregsithole@hotmail.co.za>
2022-08-02 08:49:43 +0200

    Swithced to using react-tabs

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,53 +1,56 @@
-import React from 'react';
-import './Resume.css';
-import Tab from './Tab';
-import Tabs from './Tabs';
-import TabList from './TabList';
-import Button from './Button';
-import TabPanel from './TabPanel';
+import React, { useState } from "react";
+import "./Resume.css";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import Button from "./Button";
 
-import WorkExperience from './WorkExperience';
-
+import WorkExperience from "./WorkExperience";
 
 const Resume = () => {
-    let selected = 1;
+    const [tabIndex, setTabIndex] = useState(0);
+
     return (
         <section className="resume container section" id="resume">
             <h2 className="section__title">Experience</h2>
 
             <div className="resume__container">
-                <Tabs selected={selected}>
-                    <TabList>
-                        {WorkExperience.map((experience) => {
+                <Tabs
+                    className="tabs"
+                    selectedIndex={tabIndex}
+                    onSelect={(index) => setTabIndex(index)}
+                    selectedTabClassName={"is-active"}
+                    selectedTabPanelClassName={"is-active"}
+                >
+                    <TabList className="tab__list">
+                        {WorkExperience.map((experience, index) => {
                             const { id, company } = experience;
                             return (
-                                <Tab key={`company-${id}`}>
+                                <Tab className="tab" key={`company-${id}`}>
                                     <Button>{company}</Button>
                                 </Tab>
-                            )
+                            );
                         })}
                     </TabList>
 
                     {WorkExperience.map((experience) => {
                         const { id, company, yearsActive, title, information } = experience;
                         return (
-                            <TabPanel key={`panel-${id}`}>
-                                <h2 className="tab__panel-title">{title} @ {company}</h2>
+                            <TabPanel className="tab__panel" key={`panel-${id}`}>
+                                <h2 className="tab__panel-title">
+                                    {title} @ {company}
+                                </h2>
                                 <p className="tab__panel-subtitle">{yearsActive}</p>
                                 <ul className="tab__panel-list">
                                     {information.map((info, index) => {
-                                        return (
-                                            <li key={`info-${index}`}>{info}</li>
-                                        )
+                                        return <li key={`info-${index}`}>{info}</li>;
                                     })}
                                 </ul>
                             </TabPanel>
-                        )
+                        );
                     })}
                 </Tabs>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Resume
+export default Resume;
