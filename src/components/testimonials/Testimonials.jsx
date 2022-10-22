59dda3e5e41307a419855693f302b71072e42af4
Greg Sithole <gregsithole@hotmail.co.za>
2022-10-22 18:28:17 +0200

    Changed Testimonial

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -11,52 +11,50 @@ import 'swiper/css/pagination';
 
 const Testimonials = () => {
 
-    const data = [
-        {
-            id: 1,
-            image: Image3,
-            title: "Thapelo Kekana",
-            subtitle: "Frontend Developer @ FNB",
-            comment:
-                "I enjoy working with the theme and learn so much. You       guys make the process fun and interesting. Good luck! 👍",
-        },
-        {
-            id: 2,
-            image: Image4,
-            title: "Deepak Reddy",
-            subtitle: "Software Developer @ BMW Group South Africa",
-            comment:
-                "I enjoy working with the theme and learn so much. You       guys make the process fun and interesting. Good luck! 👍",
-        },
-    ];
+	const data = [
+		{
+			id: 1,
+			image: Image4,
+			title: "Kerren Ortlepp",
+			subtitle: "Founder @ Entrostat",
+			comment: "Greg worked at Entrostat as a full-stack developer. He was excellent and had a real eye for design. If there was ever a time where we had a complicated frontend or needed to know what the best CSS approach was for something - Greg was our goto man!",
+		},
+		{
+			id: 2,
+			image: Image3,
+			title: "Thapelo Kekana",
+			subtitle: "Frontend Developer @ FNB",
+			comment: "I enjoy working with the theme and learn so much. You guys make the process fun and interesting. Good luck! 👍",
+		},
+	];
 
-    return (
-        <section className="testimonials container section">
+	return (
+		<section className="testimonials container section">
 
-            <h2 className="section__title">Testimonials</h2>
+			<h2 className="section__title">Testimonials</h2>
 
-            <Swiper className="testimonial__container grid"
-                modules={[Pagination]}
-                spaceBetween={30}
-                slidesPerView={1}
-                loop={true}
-                grabCursor={true}
-                pagination={{ clickable: true }}>
-                {data.map(({ id, image, title, subtitle, comment }) => {
-                    return (
-                        <SwiperSlide className="testimonial__item" key={id}>
-                            <div className="thumb">
-                                <img src={image} alt="" />
-                            </div>
-                            <h3 className="testimonial__title">{title}</h3>
-                            <span className="subtitle">{subtitle}</span>
-                            <div className="comment">{comment}</div>
-                        </SwiperSlide>
-                    )
-                })}
-            </Swiper>
-        </section>
-    )
+			<Swiper className="testimonial__container grid"
+				modules={[Pagination]}
+				spaceBetween={30}
+				slidesPerView={1}
+				loop={true}
+				grabCursor={true}
+				pagination={{ clickable: true }}>
+				{data.map(({ id, image, title, subtitle, comment }) => {
+					return (
+						<SwiperSlide className="testimonial__item" key={id}>
+							<div className="thumb">
+								<img src={image} alt="" />
+							</div>
+							<h3 className="testimonial__title">{title}</h3>
+							<span className="subtitle">{subtitle}</span>
+							<div className="comment">{comment}</div>
+						</SwiperSlide>
+					)
+				})}
+			</Swiper>
+		</section>
+	)
 }
 
 export default Testimonials
