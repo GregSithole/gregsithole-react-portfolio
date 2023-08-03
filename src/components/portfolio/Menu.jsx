e5c452567a3ba757500b7a4d7fc5c4c55a03f011
Greg Sithole <gregsithole@hotmail.co.za>
2023-08-03 22:59:41 +0200

    Updated Porfolio Section

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -1,32 +1,31 @@
-import Work1 from "../../assets/work-1.svg";
-import Work2 from "../../assets/work-2.svg";
-import Work3 from "../../assets/work-3.svg";
+import Work1 from "../../assets/native-jobs.png";
+import Work2 from "../../assets/cryptoverse.png";
+import Work3 from "../../assets/travel.png";
 
 const Menu = [
-    {
-        id: 1,
-        image: Work1,
-        title: "Memories App",
-        category: ["Frontend", "React"],
-        url: "https://memory-app.gregsithole.com",
-        repositoryUrl: "https://github.com/GregSithole/memories-project"
-    },
-    {
-        id: 2,
-        image: Work2,
-        title: "Cryptoverse",
-        category: ["Frontend", "React"],
-        url: "https://cryptoverse.gregsithole.com",
-        repositoryUrl: "https://github.com/GregSithole/crypto-app"
-    },
-    {
-        id: 3,
-        image: Work3,
-        title: "Travel Advisor",
-        category: ["Frontend"],
-        url: "https://travel.gregsithole.com",
-        repositoryUrl: "https://github.com/GregSithole/travel-advisor"
-    }
+	{
+		id: 1,
+		image: Work1,
+		title: "React Native Jobs App",
+		category: ["Frontend", "React"],
+		repositoryUrl: "https://github.com/GregSithole/react-native-jobs"
+	},
+	{
+		id: 2,
+		image: Work2,
+		title: "Cryptoverse",
+		category: ["Frontend", "React"],
+		url: "https://cryptoverse.gregsithole.com",
+		repositoryUrl: "https://github.com/GregSithole/crypto-app"
+	},
+	{
+		id: 3,
+		image: Work3,
+		title: "Travel Advisor",
+		category: ["Frontend"],
+		url: "https://travel.gregsithole.com",
+		repositoryUrl: "https://github.com/GregSithole/travel-advisor"
+	}
 ];
 
 export default Menu;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,70 +7,70 @@ import { RiGithubLine, RiLink } from "react-icons/ri";
 import { motion } from "framer-motion";
 
 const Portfolio = () => {
-    const [items, setItems] = useState(Menu);
-    const [activeFilter, setActiveFilter] = useState(0);
-    const filterItems = (categoryItem) => {
-        const updatedItems = Menu.filter((curElem) => {
-            return curElem.category.includes(categoryItem);
-        });
+	const [items, setItems] = useState(Menu);
+	const [activeFilter, setActiveFilter] = useState(0);
+	const filterItems = (categoryItem) => {
+		const updatedItems = Menu.filter((curElem) => {
+			return curElem.category.includes(categoryItem);
+		});
 
-        setItems(updatedItems);
-    };
+		setItems(updatedItems);
+	};
 
-    return (
-        <section className="portfolio container section" id="portfolio">
-            <h2 className="section__title">Recent Projects</h2>
+	return (
+		<section className="portfolio container section" id="portfolio">
+			<h2 className="section__title">Recent Projects</h2>
 
-            <div className="portfolio__filters">
-                <span className={activeFilter === 0 ? 'portfolio__item portfolio__item-active' : 'portfolio__item'} onClick={() => { setItems(Menu); setActiveFilter(0) }}>
-                    All
-                </span>
-                <span className={activeFilter === 1 ? 'portfolio__item portfolio__item-active' : 'portfolio__item'} onClick={() => { filterItems("Frontend"); setActiveFilter(1) }}>
-                    Frontend
-                </span>
-                <span className={activeFilter === 2 ? 'portfolio__item portfolio__item-active' : 'portfolio__item'} onClick={() => { filterItems("Backend"); setActiveFilter(2) }}>
-                    Backend
-                </span>
-                <span className={activeFilter === 3 ? 'portfolio__item portfolio__item-active' : 'portfolio__item'} onClick={() => { filterItems("Angular"); setActiveFilter(3) }}>
-                    Angular
-                </span>
-                <span className={activeFilter === 4 ? 'portfolio__item portfolio__item-active' : 'portfolio__item'} onClick={() => { filterItems("React"); setActiveFilter(4) }}>
-                    React
-                </span>
-            </div>
+			<div className="portfolio__filters">
+				<span className={activeFilter === 0 ? 'portfolio__item portfolio__item-active' : 'portfolio__item'} onClick={() => { setItems(Menu); setActiveFilter(0) }}>
+					All
+				</span>
+				<span className={activeFilter === 1 ? 'portfolio__item portfolio__item-active' : 'portfolio__item'} onClick={() => { filterItems("Frontend"); setActiveFilter(1) }}>
+					Frontend
+				</span>
+				<span className={activeFilter === 2 ? 'portfolio__item portfolio__item-active' : 'portfolio__item'} onClick={() => { filterItems("Backend"); setActiveFilter(2) }}>
+					Backend
+				</span>
+				<span className={activeFilter === 3 ? 'portfolio__item portfolio__item-active' : 'portfolio__item'} onClick={() => { filterItems("Angular"); setActiveFilter(3) }}>
+					Angular
+				</span>
+				<span className={activeFilter === 4 ? 'portfolio__item portfolio__item-active' : 'portfolio__item'} onClick={() => { filterItems("React"); setActiveFilter(4) }}>
+					React
+				</span>
+			</div>
 
-            <div className="portfolio__container grid">
-                {items.map((elem) => {
-                    const { id, image, title, category, url, repositoryUrl } = elem;
+			<div className="portfolio__container grid">
+				{items.map((elem) => {
+					const { id, image, title, category, url, repositoryUrl } = elem;
 
-                    return (
-                        <motion.div
-                            layout
-                            animate={{ opacity: 1 }}
-                            initial={{ opacity: 0 }}
-                            exit={{ opacity: 0 }}
-                            transition={{ duration: 0.3 }}
-                            className="portfolio__card"
-                            key={id}>
-                            <div className="portfolio__thumbnail">
-                                <img src={image} alt="" className="portfolio__img" />
-                                <div className="portfolio__mask"></div>
-                            </div>
+					return (
+						<motion.div
+							layout
+							animate={{ opacity: 1 }}
+							initial={{ opacity: 0 }}
+							exit={{ opacity: 0 }}
+							transition={{ duration: 0.3 }}
+							className="portfolio__card"
+							key={id}>
+							<div className="portfolio__thumbnail">
+								<img src={image} alt="" className="portfolio__img" height="267" />
+								<div className="portfolio__mask"></div>
+							</div>
 
-                            <span className="portfolio__category">{category.join(', ')}</span>
-                            <h3 className="portfolio__title">{title}</h3>
-                            <a href={url} target="_blank" rel="noreferrer" className="portfolio__button">
-                                <RiLink className="portfolio__button-icon" />
-                            </a>
-                            <a href={repositoryUrl} target="_blank" rel="noreferrer" className="portfolio__github-button">
-                                <RiGithubLine className="portfolio__button-icon" />
-                            </a>
-                        </motion.div>
-                    );
-                })}
-            </div>
-        </section>
-    );
+							<span className="portfolio__category">{category.join(', ')}</span>
+							<h3 className="portfolio__title">{title}</h3>
+							<a href={url} target="_blank" rel="noreferrer" className="portfolio__button">
+								<RiLink className="portfolio__button-icon" />
+							</a>
+							<a href={repositoryUrl} target="_blank" rel="noreferrer" className="portfolio__github-button">
+								<RiGithubLine className="portfolio__button-icon" />
+							</a>
+						</motion.div>
+					);
+				})}
+			</div>
+		</section>
+	);
 };
 
 export default Portfolio;
